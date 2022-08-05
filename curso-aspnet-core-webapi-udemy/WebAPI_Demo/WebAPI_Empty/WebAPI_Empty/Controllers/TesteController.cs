18b7f0a29100a8f474ff3e30f5c85a2cebcf78fb
Jorge Clênio <cleniocontato@gmail.com>
2022-08-05 17:09:55 -0300

    05/08/2022

---
+++ curso-aspnet-core-webapi-udemy/WebAPI_Demo/WebAPI_Empty/WebAPI_Empty/Controllers/TesteController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI_Empty.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "jorge", "clenio" };
+        }
+
+        [HttpGet("pessoas")]
+        public ActionResult<IEnumerable<Pessoa>> GetPessoas()
+        {
+            return new[]
+            {
+                new Pessoa { Nome = "Felipe"},
+                new Pessoa { Nome = "Marcos"},
+                new Pessoa { Nome = "Pedro"}
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+    }
+}
