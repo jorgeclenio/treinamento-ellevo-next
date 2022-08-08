0383f13dde7987c1e3e119111f33a4d46f73141d
Jorge Clênio <cleniocontato@gmail.com>
2022-08-08 10:29:32 -0300

    08/08/2022

--- curso-aspnet-core-webapi-udemy/APICatalogo/APICatalogo/APICatalogo/Controllers/CategoriasController.cs
+++ curso-aspnet-core-webapi-udemy/APICatalogo/APICatalogo/APICatalogo/Controllers/CategoriasController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace APICatalogo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CategoriasController : ControllerBase
     {
         private readonly AppDbContext _context;
--- curso-aspnet-core-webapi-udemy/APICatalogo/APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ curso-aspnet-core-webapi-udemy/APICatalogo/APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace APICatalogo.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProdutosController : ControllerBase
     {
         private readonly AppDbContext _context;
