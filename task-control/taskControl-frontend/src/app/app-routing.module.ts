23c29fad33d35aa28404a5eb509ce9f33c3358c1
Jorge Clênio <cleniocontato@gmail.com>
2022-08-16 09:45:43 -0300

    16/08/2022

--- task-control/taskControl-frontend/src/app/app-routing.module.ts
+++ task-control/taskControl-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent,
+    data: { title: "login" },
+  },
+  {
+    path: "",
+    redirectTo: "/login",
+    pathMatch: "full",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
