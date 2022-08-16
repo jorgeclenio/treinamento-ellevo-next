5a77523049385d3022f661ea0f427afa79a4fc11
Jorge Clênio <cleniocontato@gmail.com>
2022-08-16 11:25:03 -0300

    16/08/2022

--- task-control/taskControl-frontend/src/app/app-routing.module.ts
+++ task-control/taskControl-frontend/src/app/app-routing.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { LoginComponent } from "./login/login.component";
+import { UsersComponent } from "./users/users.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 const routes: Routes = [
-  {
-    path: "login",
-    component: LoginComponent,
-    data: { title: "login" },
-  },
-  {
-    path: "",
-    redirectTo: "/login",
-    pathMatch: "full",
-  },
+  { path: "", redirectTo: "/", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "users", component: UsersComponent },
+  { path: "tasks", component: TasksComponent },
 ];
 
 @NgModule({
--- task-control/taskControl-frontend/src/app/app.module.ts
+++ task-control/taskControl-frontend/src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { NavComponent } from "./nav/nav.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { UsersComponent } from "./users/users.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavComponent,
+    ProfileComponent,
+    UsersComponent,
+    TasksComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
