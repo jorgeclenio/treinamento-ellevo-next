6156181ec1bc9cfdb446e8dcd33397ebbe314184
Jorge Clênio <cleniocontato@gmail.com>
2022-10-28 14:58:53 -0300

    28/10/2022

--- curso-angular-udemy/main/src/app/app-routing.module.ts
+++ curso-angular-udemy/main/src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { AboutComponent } from "./pages/about/about.component";
-import { HomeComponent } from "./pages/home/home.component";
-import { NotFoundComponent } from "./pages/not-found/not-found.component";
-
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "not-found", component: NotFoundComponent },
-  { path: "**", redirectTo: "not-found" },
+  // { path: "", redirectTo: "home", pathMatch: "full" },
+  // { path: "home", component: HomeComponent },
+  // { path: "about", component: AboutComponent },
+  // { path: "not-found", component: NotFoundComponent },
+  // { path: "**", redirectTo: "not-found" },
 ];
 
 @NgModule({
--- curso-angular-udemy/main/src/app/app.module.ts
+++ curso-angular-udemy/main/src/app/app.module.ts
@@ -1,26 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { AboutComponent } from './pages/about/about.component';
-import { TodoComponent } from './pages/todo/todo.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NotFoundComponent,
-    AboutComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
