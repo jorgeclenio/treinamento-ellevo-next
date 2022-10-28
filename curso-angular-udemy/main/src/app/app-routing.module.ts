91688945b1e3e8e5c755e3279014dc70b0294a55
Jorge Clênio <cleniocontato@gmail.com>
2022-10-28 17:48:27 -0300

    28/10/2022

--- curso-angular-udemy/main/src/app/app-routing.module.ts
+++ curso-angular-udemy/main/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { HomeComponent } from "./home/home.component";
+import { ProjectsComponent } from "./modules/projects/projects.component";
+
+import { AboutComponent } from "./shared/pages/about/about.component";
+import { NotFoundComponent } from "./shared/pages/not-found/not-found.component";
+
 const routes: Routes = [
-  // { path: "", redirectTo: "home", pathMatch: "full" },
-  // { path: "home", component: HomeComponent },
-  // { path: "about", component: AboutComponent },
-  // { path: "not-found", component: NotFoundComponent },
-  // { path: "**", redirectTo: "not-found" },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "not-found", component: NotFoundComponent },
+  { path: "**", redirectTo: "not-found" },
 ];
 
 @NgModule({
--- curso-angular-udemy/main/src/app/app.module.ts
+++ curso-angular-udemy/main/src/app/app.module.ts
@@ -4,8 +4,26 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
+import { HomeComponent } from "./home/home.component";
+
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { AboutComponent } from "./shared/pages/about/about.component";
+import { NotFoundComponent } from "./shared/pages/not-found/not-found.component";
+import { ProjectsComponent } from './modules/projects/projects.component';
+
+
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    NotFoundComponent,
+    ProjectsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
